e68298f7016d013cec2b17e39bec6b8261af6a16
BlaBla <pattarasai.markpeng@gmail.com>
2016-04-23 21:59:54 +0700

    Add display block on scrollElement

--- build/virtual-scroll/component/virtual-scroll-controller.js
+++ build/virtual-scroll/component/virtual-scroll-controller.js
@@ -5,11 +5,15 @@ var VirtualScroll = (function () {
     }
     VirtualScroll.prototype.inititalComponent = function () {
         this.initialRootElement();
+        this.initialInnerElement();
     };
     VirtualScroll.prototype.initialRootElement = function () {
         this.scrollElement = this.$element.find("scrollElement");
         this.scrollElement.css("height", "200px");
         this.scrollElement.css("overflow", "auto");
+        this.scrollElement.css("display", "block");
+    };
+    VirtualScroll.prototype.initialInnerElement = function () {
     };
     VirtualScroll.prototype.setType = function () {
         console.log("called");
--- src/virtual-scroll/component/virtual-scroll-controller.ts
+++ src/virtual-scroll/component/virtual-scroll-controller.ts
@@ -3,20 +3,27 @@
 class VirtualScroll {
     $onInit:Function;
     scrollElement:any;
+    innerScrollElement:any;
     constructor(private $element:angular.IRootElementService) {
       this.$onInit = this.inititalComponent;
     }
 
     inititalComponent() {
         this.initialRootElement();
+        this.initialInnerElement();
     }
 
     initialRootElement() {
       this.scrollElement = this.$element.find("scrollElement");
       this.scrollElement.css("height", "200px");
       this.scrollElement.css("overflow", "auto");
+        this.scrollElement.css("display", "block");
     }
 
+    initialInnerElement() {
+      //this.innerScrollElement =  this.$element.find("innerScrollElement");
+      //this.innerScrollElement.css("height", "1000px");
+    }
     setType() {
       console.log("called");
     }
